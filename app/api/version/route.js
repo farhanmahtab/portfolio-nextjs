a1df3972301b243949abc5dddeb83b671c878a84
Mahi <farhan.mahi@gmail.com>
2026-01-07 00:44:09 +0600

    Build time updated

--- app/api/version/route.js
+++ app/api/version/route.js
@@ -1,10 +1,9 @@
 export const getVersion = () => {
-  const apiEnv = process.env.APP_ENV || process.env.NODE_ENV;
   return new Response(
     JSON.stringify({
       name: "Portfolio",
-      version: "2.0.0",
-      "build-time": "2026-01-6 10.00:00PM BST",
+      version: "2.0.1",
+      "build-time": "2026-01-7 12.45:00PM BST",
       "current-time": new Date().toISOString(),
     }),
     {
